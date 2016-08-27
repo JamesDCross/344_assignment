acc1a0dc34864b3de1a256a577478514e8f56d0f
zwpdbh <hyperion_z@outlook.com>
2016-08-27 17:53:40 +1200

    <update load.sql and queries.sql <fixed some bug and drop table order. need to check table creation that the datatype and the data value should be match.

--- load.sql
+++ load.sql
@@ -1,28 +1,17 @@
-DROP TABLE supplies;
-DROP TABLE book_tran;
 DROP TABLE qualifications_type;
 DROP TABLE qualifications;
+DROP TABLE book_tran;
 DROP TABLE transactions;
 DROP TABLE customer;
 DROP TABLE postcode;
 DROP TABLE employee;
 DROP TABLE employee_wage;
+DROP TABLE supplies;
 DROP TABLE supplier;
-DROP table bookstore;
 DROP TABLE book;
+DROP table bookstore;
 
 
-CREATE TABLE supplier
-(bank_account_number    CHAR(18)    PRIMARY KEY,
-company_name VARCHAR2(30) NOT NULL,
-contact_number CHAR(10));
-
-INSERT INTO supplier VALUES
-('39-6443-9454523-48','National Scholastic','034191123');
-INSERT INTO supplier VALUES
-('33-3456-9482743-32','Mills and Boon','035763832');
-INSERT INTO supplier VALUES
-('26-6843-4856284-68','Paperback Writers','04457484');
 
 
 
@@ -44,7 +33,6 @@ INSERT INTO bookstore VALUES
 ('Gore', '11 Bogan Street', '0854-6354-9142', TO_DATE('24-12-2001','DD-MM-YYYY'));
 INSERT INTO bookstore VALUES
 ('Temuka', '69 Kina Ave',  '0534-9143-6453', TO_DATE('31-10-2009','DD-MM-YYYY'));
-
 
 
 
@@ -73,6 +61,33 @@ INSERT INTO book VALUES
 ('Lion Tamming', '323-3-323434-76-7', 'Claud','B','Ottem','2950', '20');
 
 
+
+CREATE TABLE supplier
+(bank_account_number    CHAR(18)    PRIMARY KEY,
+company_name VARCHAR2(30) NOT NULL,
+contact_number CHAR(10));
+
+INSERT INTO supplier VALUES
+('39-6443-9454523-48','National Scholastic','034191123');
+INSERT INTO supplier VALUES
+('33-3456-9482743-32','Mills and Boon','035763832');
+INSERT INTO supplier VALUES
+('26-6843-4856284-68','Paperback Writers','04457484');
+
+
+
+CREATE TABLE supplies
+(sbank_account_number CHAR(18) REFERENCES supplier (bank_account_number),
+bisbn CHAR(17) REFERENCES book(isbn),
+CONSTRAINT supplies_pk PRIMARY KEY(sbank_account_number, bisbn));
+
+INSERT INTO supplies VALUES
+('39-6443-9454523-48','326-1-234923-21-2');
+INSERT INTO supplies VALUES
+('39-6443-9454523-48','321-1-234333-21-8');
+
+
+
 CREATE TABLE employee_wage
 (weekly_hours   NUMBER(2),
 hourly_rate    NUMBER(5),
@@ -80,8 +95,14 @@ wage NUMBER (8) default 1000,
 CONSTRAINT employee_wage_pk PRIMARY KEY(weekly_hours, hourly_rate));
 
 INSERT INTO employee_wage VALUES('38', '1525','57950');
+INSERT INTO employee_wage VALUES('38', '1600','60800');
+INSERT INTO employee_wage VALUES('38', '1700','64600');
 INSERT INTO employee_wage VALUES('32', '1800','57600');
-
+INSERT INTO employee_wage VALUES('32', '1900','60800');
+INSERT INTO employee_wage VALUES(40, 1500, 60000);
+INSERT INTO employee_wage VALUES(40, 1700, 68000);
+INSERT INTO EMPLOYEE_WAGE VALUES(35, 1750, 61250);
+INSERT INTO EMPLOYEE_WAGE VALUES(35, 1850, 64750);
 
 
 
@@ -100,23 +121,21 @@ INSERT INTO employee VALUES
 ('John','B','Good', '024-613-323',       '0212344505', 38, 1525, (select address from (select * from bookstore order by DBMS_RANDOM.RANDOM) WHERE rownum < 2));
 INSERT INTO employee VALUES
 ('Debbie','D','Dallas', '023-543-765',   '0215739542', 32, 1800, (select address from (select * from bookstore order by DBMS_RANDOM.RANDOM) WHERE rownum < 2));
+INSERT INTO employee VALUES
+('Tom','N','Mates', '013-643-923',       '0273938492', '38', 1600, (select address from (select * from bookstore order by DBMS_RANDOM.RANDOM) WHERE rownum < 2));
+INSERT INTO employee VALUES
+('Rebbecca','B','Smith', '075-142-345',  '0214847395', '40', '1700', (select address from (select * from bookstore order by DBMS_RANDOM.RANDOM) WHERE rownum < 2));
+INSERT INTO employee VALUES
+('Greg','T','Somerville', '087-681-765', '0270394722', '40', '1500', (select address from (select * from bookstore order by DBMS_RANDOM.RANDOM) WHERE rownum < 2));
+INSERT INTO employee VALUES
+('Todd','A','BlackAdder', '091-813-913', '0215738375', '35', '1750', (select address from (select * from bookstore order by DBMS_RANDOM.RANDOM) WHERE rownum < 2));
+INSERT INTO employee VALUES
+('Wayne','','Kerr', '023-842-366',       '0273231043', '35', '1850', (select address from (select * from bookstore order by DBMS_RANDOM.RANDOM) WHERE rownum < 2));
+INSERT INTO employee VALUES
+('Leroy','J','Brown', '073-834-552',     '0215551234', '32', '1900', (select address from (select * from bookstore order by DBMS_RANDOM.RANDOM) WHERE rownum < 2));
+INSERT INTO employee VALUES
+('Ying','G','Brown', '055-923-819',      '0214984833', '38', '1600', (select address from (select * from bookstore order by DBMS_RANDOM.RANDOM) WHERE rownum < 2));
 
-/**
-INSERT INTO employee VALUES
-('Tom','N','Mates', '013-643-923',       '0273938492', '32', 1800, (select address from (select * from bookstore order by DBMS_RANDOM.RANDOM) WHERE rownum < 2));
-INSERT INTO employee VALUES
-('Rebbecca','B','Smith', '075-142-345',  '0214847395', '38', '1525', (select address from (select * from bookstore order by DBMS_RANDOM.RANDOM) WHERE rownum < 2));
-INSERT INTO employee VALUES
-('Greg','T','Somerville', '087-681-765', '0270394722', '38', '2150', (select address from (select * from bookstore order by DBMS_RANDOM.RANDOM) WHERE rownum < 2));
-INSERT INTO employee VALUES
-('Todd','A','BlackAdder', '091-813-913', '0215738375', '15', '1925', (select address from (select * from bookstore order by DBMS_RANDOM.RANDOM) WHERE rownum < 2));
-INSERT INTO employee VALUES
-('Wayne','','Kerr', '023-842-366',       '0273231043', '38', '1900', (select address from (select * from bookstore order by DBMS_RANDOM.RANDOM) WHERE rownum < 2));
-INSERT INTO employee VALUES
-('Leroy','J','Brown', '073-834-552',     '0215551234', '20', '2000', (select address from (select * from bookstore order by DBMS_RANDOM.RANDOM) WHERE rownum < 2));
-INSERT INTO employee VALUES
-('Ying','G','Brown', '055-923-819',      '0214984833', '38', '1550', (select address from (select * from bookstore order by DBMS_RANDOM.RANDOM) WHERE rownum < 2));
-*/
 
 
 
@@ -139,8 +158,10 @@ INSERT INTO postcode VALUES
 INSERT INTO postcode VALUES
 (9012, 'South Dunedin', 'Dunedin');
 
+
+
 CREATE TABLE customer
-(street_number VARCHAR2(5),
+(street_number NUMBER(5),
 Street_name VARCHAR2(30),
 postcode NUMBER(4) NOT NULL REFERENCES postcode(postcode),
 customer_id CHAR (7)            PRIMARY KEY,
@@ -150,35 +171,35 @@ phone CHAR (10)
 );
 
 INSERT INTO customer VALUES
-('22', 'Strawberry Lane', 9016, '0007365','Chubby','Checker', '0212546798');
+(22, 'Strawberry Lane', 9016, '0007365','Chubby','Checker', '0212546798');
 INSERT INTO customer VALUES
-('16', 'Princes Street', 9012, '0046290','Slim','Dusty', '0279843873');
+(16, 'Princes Street', 9012, '0046290','Slim','Dusty', '0279843873');
+INSERT INTO customer VALUES
+(11, 'Boiling Down Road',9012,'0002233','Jimmy','Johns','');
+INSERT INTO customer VALUES
+(17,'Duchess Ave',9016,'0000010','Callum','Grimmer','0223280679');
+INSERT INTO customer VALUES
+(14,'Findlayson Road',9012,'0022056','Tim','Shadbolt','0272524455');
+INSERT INTO customer VALUES
+(25,'River Street',9016,'0024789','Joseph','Parker','');
+INSERT INTO customer VALUES
+(560,'George Street',9016,'0001010','David','Clark','');
+INSERT INTO customer VALUES
+(34, 'Midland Street',9012,'0033210','Dave','Brubeck','');
+INSERT INTO customer VALUES
+(131,'Queen Street',9016,'0035670','Trent','Brown','0223220987');
+INSERT INTO customer VALUES
+(11,'Pitt Street',9016,'0668220','Sammy','Dee','0274429824');
+INSERT INTO customer VALUES
+(2,'Wigan Street',9012,'0096210','Michael','Peterson','0211197862');
+INSERT INTO customer VALUES
+(62,'Robertson Street',9016,'0000223','Foxy','Brown','0274542211');
 
-INSERT INTO customer VALUES
-('11', 'Boiling Down Road',9012,'0002233','Jimmy','Johns','');
-INSERT INTO customer VALUES
-('17','Duchess Ave',9016,'0000010','Callum','Grimmer','0223280679');
-INSERT INTO customer VALUES
-('14','Findlayson Road',9012,'0022056','Tim','Shadbolt','0272524455');
-INSERT INTO customer VALUES
-('25','River Street',9016,'0024789','Joseph','Parker','');
-INSERT INTO customer VALUES
-('560','George Street',9016,'0001010','David','Clark','');
-INSERT INTO customer VALUES
-('34', 'Midland Street',9012,'0033210','Dave','Brubeck','');
-INSERT INTO customer VALUES
-('131a','Queen Street',9016,'0035670','Trent','Brown','0223220987');
-INSERT INTO customer VALUES
-('11','Pitt Street',9016,'0668220','Sammy','Dee','0274429824');
-INSERT INTO customer VALUES
-('2','Wigan Street',9012,'0096210','Michael','Peterson','0211197862');
-INSERT INTO customer VALUES
-('62','Robertson Street',9016,'0000223','Foxy','Brown','0274542211');
 
 
 CREATE TABLE transactions
-("date" DATE NOT NULL,
-"time" DATE NOT NULL,
+(TDATE DATE NOT NULL,
+ TTIME DATE NOT NULL,
 transaction_number CHAR (17)    PRIMARY KEY,
 eird_number CHAR(11)  REFERENCES employee (ird_number),
 ccustomer_id CHAR (7) REFERENCES customer (customer_id)
@@ -217,6 +238,55 @@ INSERT INTO transactions VALUES
 (TO_DATE('22-03-2014','DD-MM-YYYY'), TO_DATE('14:03:40','hh24:mi:ss'), '000-0000-1234-204',
        (select IRD_NUMBER from (select * from employee order by DBMS_RANDOM.RANDOM) WHERE ROWNUM < 2),
        (select customer_id from (select * from customer order by DBMS_RANDOM.RANDOM) WHERE ROWNUM < 2));
+
+
+
+CREATE TABLE book_tran
+(bisbn CHAR (17)   REFERENCES book(isbn),
+ttransaction_number CHAR (17)  REFERENCES transactions(transaction_number),
+CONSTRAINT book_tran_pk PRIMARY KEY(bisbn, ttransaction_number));
+
+INSERT INTO book_tran VALUES(
+	(select isbn from (select * from book order by dbms_random.random) where rownum < 2),
+	(select TRANSACTION_NUMBER from (select TRANSACTION_NUMBER, rownum as rn from transactions order by dbms_random.random) where rn=1)
+);
+INSERT INTO book_tran VALUES(
+	(select isbn from (select * from book order by dbms_random.random) where rownum < 2),
+	(select TRANSACTION_NUMBER from (select TRANSACTION_NUMBER, rownum as rn from transactions order by dbms_random.random) where rn=2)
+);
+INSERT INTO book_tran VALUES(
+	(select isbn from (select * from book order by dbms_random.random) where rownum < 2),
+	(select TRANSACTION_NUMBER from (select TRANSACTION_NUMBER, rownum as rn from transactions order by dbms_random.random) where rn=3)
+);
+INSERT INTO book_tran VALUES(
+	(select isbn from (select * from book order by dbms_random.random) where rownum < 2),
+	(select TRANSACTION_NUMBER from (select TRANSACTION_NUMBER, rownum as rn from transactions order by dbms_random.random) where rn=4)
+);
+INSERT INTO book_tran VALUES(
+	(select isbn from (select * from book order by dbms_random.random) where rownum < 2),
+	(select TRANSACTION_NUMBER from (select TRANSACTION_NUMBER, rownum as rn from transactions order by dbms_random.random) where rn=5)
+);
+INSERT INTO book_tran VALUES(
+	(select isbn from (select * from book order by dbms_random.random) where rownum < 2),
+	(select TRANSACTION_NUMBER from (select TRANSACTION_NUMBER, rownum as rn from transactions order by dbms_random.random) where rn=6)
+);
+INSERT INTO book_tran VALUES(
+	(select isbn from (select * from book order by dbms_random.random) where rownum < 2),
+	(select TRANSACTION_NUMBER from (select TRANSACTION_NUMBER, rownum as rn from transactions order by dbms_random.random) where rn=7)
+);
+INSERT INTO book_tran VALUES(
+	(select isbn from (select * from book order by dbms_random.random) where rownum < 2),
+	(select TRANSACTION_NUMBER from (select TRANSACTION_NUMBER, rownum as rn from transactions order by dbms_random.random) where rn=8)
+);
+/**
+INSERT INTO book_tran VALUES
+('326-1-234923-21-2', '000-0000-1434-455');
+INSERT INTO book_tran VALUES
+('321-1-234333-21-8', '000-0000-1234-125');
+*/
+
+
+
 
 CREATE TABLE qualifications (
 eird_number CHAR (11) REFERENCES EMPLOYEE(ird_number),
@@ -260,44 +330,4 @@ INSERT INTO qualifications_type VALUES
 ('023-842-366', 'BSci', 'bachelors degree');
 */
 
-CREATE TABLE supplies
-(sbank_account_number CHAR(18) REFERENCES supplier (bank_account_number),
-bisbn CHAR(17) REFERENCES book(isbn),
-CONSTRAINT supplies_pk PRIMARY KEY(sbank_account_number, bisbn));
 
-
-
-INSERT INTO supplies VALUES
-('39-6443-9454523-48','326-1-234923-21-2');
-INSERT INTO supplies VALUES
-('39-6443-9454523-48','321-1-234333-21-8');
-
-
-
-
-CREATE TABLE book_tran
-(bisbn CHAR (17)   REFERENCES book(isbn),
-ttransaction_number CHAR (17)  REFERENCES transactions(transaction_number),
-CONSTRAINT book_tran_pk PRIMARY KEY(bisbn, ttransaction_number));
-
-
-INSERT INTO book_tran VALUES(
-	(select isbn from (select * from book order by dbms_random.random) where rownum < 2),
-	(select TRANSACTION_NUMBER from (select * from transactions order by dbms_random.random) where rownum < 2)
-);
-
-INSERT INTO book_tran VALUES(
-	(select isbn from (select * from book order by dbms_random.random) where rownum < 2),
-	(select TRANSACTION_NUMBER from (select * from transactions order by dbms_random.random) where rownum < 2)
-);
-
-INSERT INTO book_tran VALUES(
-	(select isbn from (select * from book order by dbms_random.random) where rownum < 2),
-	(select TRANSACTION_NUMBER from (select * from transactions order by dbms_random.random) where rownum < 2)
-);
-/**
-INSERT INTO book_tran VALUES
-('326-1-234923-21-2', '000-0000-1434-455');
-INSERT INTO book_tran VALUES
-('321-1-234333-21-8', '000-0000-1234-125');
-*/
--- queries.sql
+++ queries.sql
@@ -1,49 +1,54 @@
-
-/*note these queries may need improvement*/
-q1
+/**
+q01
+*/
 SELECT title FROM book
 WHERE title LIKE 'S%';
+/**
+q02
+*/
+select * from bookstore 
+where date_opened = TO_DATE('22-05-1999', 'dd-mm-yyyy');
+/**
+q03
+*/
+select max(hourly_rate), weekly_hours 
+from employee_wage 
+group by weekly_hours 
+having max(hourly_rate) < 1800;
+/**
+q04
+show employee and their hourly_rate which is smaller than average hourly_rate
+*/
+select fname, lname, hourly_rate 
+from employee 
+where hourly_rate < (
+	select avg(hourly_rate) 
+	from employee_wage);
+/**
+q05
+show first name, last name of customer who purchase item at '22-05-2013'
+*/
+select fname, lname from customer c 
+where to_date('22-05-2013', 'dd-mm-yyyy') 
+IN (select t.tdate 
+	from transactions t 
+	where c.CUSTOMER_ID = t.CCUSTOMER_ID);
+/**
+q06
+show sale number  of books according to book_tran info
+*/
+select count(BISBN), BISBN from book_tran group by BISBN;
 
-q2
-SELECT * FROM bookstore
-WHERE date_opened  = TO_DATE('08-01-1998','DD-MM-YYYY'));
-
-q3
-SELECT hour_rate, MIN(week_hours)
-FROM employee_wage
-GROUP BY week_hours
-HAVING MIN(week_hours) <= 30;
-
-
-q4
-SELECT *
-FROM employee
-WHERE hourly_rate <
-(SELECT hourly_rate
-FROM employee
-WHERE fname = 'Debbie' AND lname = 'Dallas');
-
-/*select all in bookstore where city = milton and addresses of employees and bookstore
-are the same*/
-q5
-SELECT *
-FROM bookstore b
-WHERE city = 'Milton' IN
-(SELECT baddress
-FROM employee  e
-WHERE b.address=e.baddress);
-
-q6
-SELECT COUNT(*) FROM post_code;
-
-q7
+/**
+q07
+*/
 DELETE FROM customer
-WHERE street_number > 22
-AND lname LIKE 'B%';
-
-q8
-/*needs to be expanded on*/
-SELECT lname
-FROM customer, employee, book;
-
-
+WHERE STREET_NUMBER = 22
+AND lname LIKE 'C%';
+/**
+q08
+select fname, lname of employee who handled transactions involve customer lives in "9012 South Dunedin"
+*/
+select e.fname, e.lname
+from employee e, transactions t, customer c 
+where c.postcode=9012 and c.CUSTOMER_ID = t.CCUSTOMER_ID and t.EIRD_NUMBER = e.IRD_NUMBER;
